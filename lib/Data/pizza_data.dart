0451599bee584b2d53d1c64f945138aa7d15a972
ScionRs <grazs116@gmail.com>
2022-10-20 18:40:15 +0300

    Added json library

--- lib/Data/pizza_data.dart
+++ lib/Data/pizza_data.dart
@@ -1,11 +1,11 @@
-
-
 class PizzaData{
   final String imageName;
   final String imageDetail;
   final String title;
   final String description;
   final int price;
+
+
 
   PizzaData({required this.imageName,required this.imageDetail,required this.title,required this.description,required this.price});
 }
