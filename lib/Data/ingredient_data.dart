048712ed0a20710ff5069a0a216dff655df251ee
ScionRs <grazs116@gmail.com>
2022-10-31 11:06:04 +0300

    Activate _id and imageName fields in ingredient_data

--- lib/Data/ingredient_data.dart
+++ lib/Data/ingredient_data.dart
@@ -3,15 +3,16 @@ import 'package:json_annotation/json_annotation.dart';
 
 @JsonSerializable()
 class IngredientOptionalData{
-  //final String id;
-  //final String imgName;
+  final String id;
+  final String imgName;
   final String title;
   final int price;
 
-  IngredientOptionalData({/*required this.id,*/ /*required this.imgName,*/ required this.title, required this.price});
+  IngredientOptionalData({required this.id, required this.imgName, required this.title, required this.price});
   factory IngredientOptionalData.fromJson(Map<String, dynamic> parsedJson){
     return IngredientOptionalData(
-      //imgName: parsedJson['imgName'],
+      id: parsedJson['_id'],
+      imgName: parsedJson['imgName'],
       title: parsedJson['title'],
       price: parsedJson['price'],
     );
