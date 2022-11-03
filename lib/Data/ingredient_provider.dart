4e9da4f579bea5d408da621333f965db8f6df391
ScionRs <grazs116@gmail.com>
2022-11-03 14:30:12 +0300

    Added Provider logic to basket button in detail_page

--- lib/Data/ingredient_provider.dart
+++ lib/Data/ingredient_provider.dart
@@ -1,21 +1,32 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:pizza_app/Data/pizza_data.dart';
 
-class IngredientProvider with ChangeNotifier{
+class IngredientProvider with ChangeNotifier {
   final List<int> _selectedPriceIngredients = [];
+  final List<PizzaData> _pizzaDataProviderList = [];
   int _resultSelectPrice = 0;
 
   int get resultSelectPrice => _resultSelectPrice;
+
   List<int> get selectedPriceIngredients => _selectedPriceIngredients;
 
-  void addToList(int value){
+  void addToList(int value) {
     _selectedPriceIngredients.add(value);
     notifyListeners();
   }
 
-  void sumValuesOfSelectedIngredients(){
-    _resultSelectPrice = _selectedPriceIngredients.reduce((value, element) => value + element);
+  void sumValuesOfSelectedIngredients() {
+    _resultSelectPrice =
+        _selectedPriceIngredients.reduce((value, element) => value + element);
     print(_resultSelectPrice);
     notifyListeners();
   }
+
+  void addToPizzaDataList(PizzaData pizzaData){
+    _pizzaDataProviderList.add(pizzaData);
+    print("From provider: $_pizzaDataProviderList");
+    notifyListeners();
+  }
+
 }
--- lib/personal_page/detail_page.dart
+++ lib/personal_page/detail_page.dart
@@ -68,164 +68,176 @@ class _DetailScreenState extends State<DetailScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-          physics: BouncingScrollPhysics(),
-        children: [
-          Container(
-          width: double.infinity,
-          child: Column(
-            children: [
-              Container(
-                height: 60,
-                width: double.infinity,
-                alignment: Alignment.center,
-                color:  Color.fromRGBO(80, 166, 132, 1),
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child:
-                      ElevatedButton(
-                        onPressed: (){
-                          Navigator.pop(context);
-                        },
-                        child: Icon(Icons.arrow_circle_left_outlined,size: 45.0, color: Colors.white),
-                        style: ElevatedButton.styleFrom(
-                          shape: CircleBorder(),
-                          primary: AppColors.mainColor, // <-- Button color
-                          onPrimary: Colors.red,
-                          // <-- Splash color
-                        ),
-                      )
-                    ),
-                    Align(
-                        alignment: Alignment.center,
-                        child: LogoWidget()),
-                  ]
-                ),
-              ),
-              Image.network(
-                widget.pizzaData.imageDetail,
-                width: double.infinity,
-                height: 300,
-                fit: BoxFit.contain,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 15.0, top: 10.0, right: 5.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    _TitleWidget(widget: widget),
-                    SizedBox(height: 10.0),
-                    _DescriptionWidget(widget: widget),
-                    SizedBox(height: 10.0),
-                    Center(
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(243, 243, 247, 1),
-                          borderRadius: const BorderRadius.all(Radius.circular(5))
-                        ),
-                        child: ToggleButtons(
-                            onPressed: (int index){
-                              setState((){
-                                for (int i = 0; i < widget.pizzaData.size.length; i++) {
-                                  _selectedPizzaSize[i] = i == index;
-                                  //pizzaPrice = pizzaSizePrice[i];
-                                  if(_selectedPizzaSize[i] = i == index){
-                                    if(priceSelectedList.contains(widget.pizzaData.size[i].price)){
-                                      priceSelectedList.add(0);
-                                    } else{
-                                      listSize.add(widget.pizzaData.size[i]);
-                                      priceSelectedList.add(widget.pizzaData.size[i].price);
-                                    }
-                                  } else {
-                                    priceSelectedList.remove(widget.pizzaData.size[i].price);
-                                    listSize.remove(widget.pizzaData.size[i]);
-                                  }
-                                }
-                              });
+    var model = context.read<IngredientProvider>();
+    return ChangeNotifierProvider(
+        create: (context) => IngredientProvider(),
+        child: Scaffold(
+          body: ListView(physics: BouncingScrollPhysics(), children: [
+            Container(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Container(
+                    height: 60,
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    color: Color.fromRGBO(80, 166, 132, 1),
+                    child: Stack(alignment: Alignment.center, children: [
+                      Align(
+                          alignment: Alignment.centerLeft,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
                             },
-                          borderRadius: const BorderRadius.all(Radius.circular(5)),
-                          selectedColor: Colors.black,
-                          selectedBorderColor: Colors.transparent,
-                          fillColor: Colors.white,
-                          color: Colors.black,
-                          hoverColor: Colors.black,
-                          borderColor: Colors.transparent,
-                          constraints: const BoxConstraints(
-                            minHeight: 40.0,
-                            minWidth: 120.0,
-                          ),
-                          isSelected: _selectedPizzaSize,
-                          children: pizzaSize,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 15.0),
-                    _AddIngredientsTitleWidget(),
-                    SizedBox(height: 15.0),
-                    GridView.builder(
-                        scrollDirection: Axis.vertical,
-                        shrinkWrap: true,
-                        physics: ScrollPhysics(),
-                        itemCount:  widget.pizzaData.ingredients.length,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3,
-                            childAspectRatio: 0.56,
-                            crossAxisSpacing: 6,
-                            mainAxisSpacing: 6,
-                        ),
-                        itemBuilder: (context, index){
-                          return IngredientItem(
-                              key: Key(widget.pizzaData.ingredients[index].price.toString()),
-                              ingredientItem: widget.pizzaData.ingredients[index],
-                              isSelected: (bool value) {
+                            child: Icon(Icons.arrow_circle_left_outlined,
+                                size: 45.0, color: Colors.white),
+                            style: ElevatedButton.styleFrom(
+                              shape: CircleBorder(),
+                              primary: AppColors.mainColor,
+                              // <-- Button color
+                              onPrimary: Colors.red,
+                              // <-- Splash color
+                            ),
+                          )),
+                      Align(alignment: Alignment.center, child: LogoWidget()),
+                    ]),
+                  ),
+                  Image.network(
+                    widget.pizzaData.imageDetail,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.contain,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 15.0, top: 10.0, right: 5.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        _TitleWidget(widget: widget),
+                        SizedBox(height: 10.0),
+                        _DescriptionWidget(widget: widget),
+                        SizedBox(height: 10.0),
+                        Center(
+                          child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: BoxDecoration(
+                                color: Color.fromRGBO(243, 243, 247, 1),
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(5))),
+                            child: ToggleButtons(
+                              onPressed: (int index) {
                                 setState(() {
-                                  if (value){
-                                    //pizzaPrice = int.parse(widget.pizzaData.price.toString());
-                                    selectedList.add(widget.pizzaData.ingredients[index]);
-                                    priceSelectedList.add(int.parse(widget.pizzaData.ingredients[index].price.toString()));
-                                  } else {
-                                    selectedList.remove(widget.pizzaData.ingredients[index]);
-                                    priceSelectedList.remove(int.parse(widget.pizzaData.ingredients[index].price.toString()));
+                                  for (int i = 0;
+                                      i < widget.pizzaData.size.length;
+                                      i++) {
+                                    _selectedPizzaSize[i] = i == index;
+                                    //pizzaPrice = pizzaSizePrice[i];
+                                    if (_selectedPizzaSize[i] = i == index) {
+                                      if (priceSelectedList.contains(
+                                          widget.pizzaData.size[i].price)) {
+                                        priceSelectedList.add(0);
+                                      } else {
+                                        listSize.add(widget.pizzaData.size[i]);
+                                        priceSelectedList.add(
+                                            widget.pizzaData.size[i].price);
+                                      }
+                                    } else {
+                                      priceSelectedList.remove(
+                                          widget.pizzaData.size[i].price);
+                                      listSize.remove(widget.pizzaData.size[i]);
+                                    }
                                   }
                                 });
-                              }
-                          );
-                        }
+                              },
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(5)),
+                              selectedColor: Colors.black,
+                              selectedBorderColor: Colors.transparent,
+                              fillColor: Colors.white,
+                              color: Colors.black,
+                              hoverColor: Colors.black,
+                              borderColor: Colors.transparent,
+                              constraints: const BoxConstraints(
+                                minHeight: 40.0,
+                                minWidth: 120.0,
+                              ),
+                              isSelected: _selectedPizzaSize,
+                              children: pizzaSize,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15.0),
+                        _AddIngredientsTitleWidget(),
+                        SizedBox(height: 15.0),
+                        GridView.builder(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            physics: ScrollPhysics(),
+                            itemCount: widget.pizzaData.ingredients.length,
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              childAspectRatio: 0.56,
+                              crossAxisSpacing: 6,
+                              mainAxisSpacing: 6,
+                            ),
+                            itemBuilder: (context, index) {
+                              return IngredientItem(
+                                  key: Key(widget
+                                      .pizzaData.ingredients[index].price
+                                      .toString()),
+                                  ingredientItem:
+                                      widget.pizzaData.ingredients[index],
+                                  isSelected: (bool value) {
+                                    setState(() {
+                                      if (value) {
+                                        //pizzaPrice = int.parse(widget.pizzaData.price.toString());
+                                        selectedList.add(widget
+                                            .pizzaData.ingredients[index]);
+                                        priceSelectedList.add(int.parse(widget
+                                            .pizzaData.ingredients[index].price
+                                            .toString()));
+                                      } else {
+                                        selectedList.remove(widget
+                                            .pizzaData.ingredients[index]);
+                                        priceSelectedList.remove(int.parse(
+                                            widget.pizzaData.ingredients[index]
+                                                .price
+                                                .toString()));
+                                      }
+                                    });
+                                  });
+                            }),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
+          ]),
+          bottomNavigationBar: OutlinedButton(
+            style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all(Size(70, 70)),
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.red),
+                  ),
+                )),
+            onPressed: () {
+              model.addToPizzaDataList(selectPizzaData());
+              print(selectPizzaData());
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) =>  MainScreenWidget()));
+            },
+            child: Text(
+              'В корзину за ${reduceSum()}',
+              style: TextStyle(color: Colors.white, fontSize: 20.0),
+            ),
           ),
-        ),
-      ]
-      ),
-      bottomNavigationBar: OutlinedButton(
-        style: ButtonStyle(
-            minimumSize: MaterialStateProperty.all(Size(70, 70)),
-            backgroundColor: MaterialStateProperty.all(Colors.red),
-            shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(
-                side: BorderSide(color: Colors.red),
-              ),
-            )
-        ),
-        onPressed: (){
-          print(selectPizzaData());
-        },
-        child:
-        Text('В корзину за ${reduceSum()}',
-          style: TextStyle(color: Colors.white,
-              fontSize: 20.0),
-        ),
-      ),
-    );
+        ));
   }
 }
 
