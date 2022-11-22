e7f43ba4aaa52b712dcd53b59db33a4ae6785707
ScionRs <grazs116@gmail.com>
2022-11-22 17:54:14 +0300

    Refactoring code in AppImages

--- lib/theme/images.dart
+++ lib/theme/images.dart
@@ -3,8 +3,4 @@ import 'package:flutter/material.dart';
 
 abstract class AppImages{
   static const logo = AssetImage('images/logo/pizza_icon.png');
-  static const event1 = 'images/carousel/event1.jpg';
-  static const event2 = 'images/carousel/event2.jpg';
-  static const event3 = 'images/carousel/event3.jpg';
-  static const pizza1 = 'images/pizza_list/pizza1.jpeg';
 }
